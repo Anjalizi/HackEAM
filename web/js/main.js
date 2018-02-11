3b7723ecf3047cd1bc2251ce2f50fc3ea2e8c0e5
Anjali Jain <aj2966@gmail.com>
2018-02-11 12:32:09 +0530

    web

--- web/js/main.js
+++ web/js/main.js
@@ -6,7 +6,50 @@ gotoByScroll();
 
 });
 
+$(document).ready(function() {
+	$(".js-example-basic-single").select2({
+		placeholder: "Select a state",
+		allowClear: true
+	});
+});
 
+$(document).ready(function() {
+	$(".js-example-basic-single2").select2({
+		placeholder: "Select a district",
+		allowClear: true
+	});
+});
+
+    
+function matchCustom(params, data) {
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+      return data;
+    }
+
+    // Do not display the item if there is no 'text' property
+    if (typeof data.text === 'undefined') {
+      return null;
+    }
+
+    // `params.term` should be the term that is used for searching
+    // `data.text` is the text that is displayed for the data object
+    if (data.text.indexOf(params.term) > -1) {
+      var modifiedData = $.extend({}, data, true);
+      modifiedData.text += ' (matched)';
+
+      // You can return modified objects from here
+      // This includes matching the `children` how you want in nested data sets
+      return modifiedData;
+    }
+
+    // Return `null` if the term should not be displayed
+    return null;
+}
+
+$(".js-example-matcher").select2({
+  matcher: matchCustom
+});
 
 var toggle_nav_container = function () {
 
